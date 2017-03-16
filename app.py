5e1445f682e36503260c7146c2198a77640e5c5d
doudino <vincent.douet@gmail.com>
2017-03-16 16:20:03 +0100

    maj sqash doc

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 
 def greetings():
+    """salut les amis"""
     print("helloo resif people 3")
 
 def repeat(x, callback):
+    """call x times callback"""
     for _ in range(x):
         callback()
 

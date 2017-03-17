e3d9b562b9739ad326002cedc0ce87e5c625fc03
Pierre v <toto@oto.fr>
2017-03-17 10:22:42 +0100

    un peu de poesie dans ce monde de fork

--- app.py
+++ app.py
@@ -1,5 +1,26 @@
 #!/usr/bin/env python
 
+"""
+
+Le loup criait
+
+Le loup criait sous les feuilles
+En crachant les belles plumes
+De son repas de volailles :
+Comme lui je me consume.
+
+Les salades, les fruits
+N'attendent que la cueillette ;
+Mais l'araignée de la haie
+Ne mange que des violettes.
+
+Que je dorme ! que je bouille
+Aux autels de Salomon.
+Le bouillon court sur la rouille,
+Et se mêle au Cédron.
+
+"""
+
 def greetings():
     """salut les amis"""
     print("helloo resif people 3")
@@ -10,4 +31,6 @@ def repeat(x, callback):
         callback()
 
 if __name__ == "__main__":
+    # blabla
     repeat(3, greetings)
+    
